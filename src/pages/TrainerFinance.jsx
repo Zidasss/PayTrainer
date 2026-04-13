a92f084ddf40a3e9652a03e35615dde10b7655bf
Gustavo Zavadniak <gustavo.zavadniak@volvo.com>
2026-04-13 12:06:26 -0300

    test

--- src/pages/TrainerFinance.jsx
+++ src/pages/TrainerFinance.jsx
@@ -193,7 +193,10 @@ export default function TrainerFinance() {
         onConfirm={openStripeDashboard}
         onCancel={() => setShowStripeConfirm(false)}
       /> 
+
       <BottomNav role="trainer" />
     </div>
   );
 }
+
+[]
